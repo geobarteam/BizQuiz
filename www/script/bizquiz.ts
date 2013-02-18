2c98afe0cfd521de324571ac5e3e736a31ce1f98
Geoffrey Vandiest <geoffrey@belgianagencies.com>
2013-02-18 21:02:03 +0100

    recompile typescript

--- www/script/bizquiz.ts
+++ www/script/bizquiz.ts
@@ -2,29 +2,72 @@
 /// <reference path="phonegap.d.ts" />
 /// <reference path="jquerymobile.d.ts" />
 
-class App {
+class App 
+{
 	static deviceReadyId : string;
+	static frontController : FrontController;
 
-	static initialize(deviceReadyId: string){
+	static initialize(deviceReadyId: string)
+	{
 		console.log("Initialize");
 		App.deviceReadyId = deviceReadyId;
 		this.bindEvents();
+		frontController = new FrontController();
 	}
 	
-	private static bindEvents () {
+	private static bindEvents () 
+	{
 		console.log("bindEvents");
 		document.addEventListener("deviceready", this.onDeviceReady, false);
 	}
 
-    private static onDeviceReady () {
+    private static onDeviceReady () 
+    {
     	console.log("onDeviceReady!!!");
 		try
 		{
-			 $.mobile.changePage( "login.html", { transition: "slideup"} );
+			 this.frontController.initialize();
 		}
     	catch(e)
     	{
     		console.log(e);
     	}
+	}
+}
+
+class FrontController 
+{
+	public user : User;
+
+	constructor()
+	{
+		this.user = new User();
+	}
+
+	public initialize()
+	{
+		if (!this.user.isAuthenticated)
+		{
+			$.mobile.changePage( "login.html", { transition: "slideup"} );
+		}
+	}
+}
+
+class User 
+{
+	public isAuthenticated : bool;
+	public name : string;
+}
+
+interface ISecurityService
+{
+	authenticate(username:string, password:string):bool;
+}
+
+class SecurityService implements ISecurityService
+{
+	public authenticate(username:string, password:string):bool
+	{
+		return username == "geobarteam" && password == "starwars";
 	}
 }
